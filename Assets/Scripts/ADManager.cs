2778402f17d07917571896d3abd9c46d7801f402
rpaano <50399801+rpaano0804@users.noreply.github.com>
2019-07-20 20:05:52 +0800

    Update

---
+++ Assets/Scripts/ADManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class ADManager : MonoBehaviour
+{
+
+    private string APP_ID = "ca-app-pub-2790492244434188~5252214182";
+
+    private BannerView bannerAD;
+    private InterstitialAd interstitialAd;
+    private RewardBasedVideoAd rewardVideoAd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //For publish apps
+        //MobileAds.Initialize(APP_ID);
+    }
+
+    void RequestBanner()
+    {
+        //for testing banners ads
+        string banner_ID = "ca-app-pub-3940256099942544/6300978111";
+        //for publishing apps
+        //string banner_ID = "ca-app-pub-2790492244434188/3113136003";
+        bannerAD = new BannerView(banner_ID, AdSize.SmartBanner , AdPosition.Top);
+
+    }
+}
